4f17399022a3ef8f04475121b7b4ec745961def7
Hlib Hodovaniuk <gleb.godovanyuk@gmail.com>
2022-12-21 22:11:14 +0200

    feat: update sidebar, add brand, price and stock filters

--- src/core/components/sidebar/index.ts
+++ src/core/components/sidebar/index.ts
@@ -1,15 +1,24 @@
 import { PageIds } from '../../../pages/app';
 import Component from '../../templates/components';
-import CategoryFilter from '../category-filter';
+import CheckboxedFilter from '../checkboxed-filter';
+import RangedFilter from '../ranged-filter';
 
 class Sidebar extends Component {
-  categoryFilter = new CategoryFilter();
+  categoryFilter = new CheckboxedFilter('category');
+  brandFilter = new CheckboxedFilter('brand');
+  priceFilter = new RangedFilter('price');
+  stockFilter = new RangedFilter('stock');
+
   constructor() {
     super('div', 'sidebar');
   }
+
   build() {
     const filters = document.createElement('div');
     const categories = document.createElement('div');
+    const brands = document.createElement('div');
+    const prices = document.createElement('div');
+    const stock = document.createElement('div');
     const reset = document.createElement('div');
     const resetButton = document.createElement('a');
     const copyLinkButton = document.createElement('a');
@@ -17,21 +26,27 @@ class Sidebar extends Component {
     filters.className = 'filters';
 
     categories.className = 'categories';
+    brands.className = 'brands';
+    prices.className = 'prices';
+    stock.className = 'stocks';
 
     reset.className = 'reset-total';
 
-    resetButton.className = 'filters__reset-btn btn';
+    resetButton.className = 'filters__reset-btn btn-filter';
     resetButton.textContent = 'Reset Filters';
     resetButton.onclick = () => (window.location.href = `#${PageIds.MainPage}`);
 
-    copyLinkButton.className = 'filters__copy-link-btn';
+    copyLinkButton.className = 'filters__copy-link-btn btn-filter';
     copyLinkButton.textContent = 'Copy Link';
     // TODO: Add onclick eventlistener
     reset.append(resetButton, copyLinkButton);
 
     categories.append(this.categoryFilter.render());
+    brands.append(this.brandFilter.render());
+    prices.append(this.priceFilter.render());
+    stock.append(this.stockFilter.render());
 
-    filters.append(reset, categories);
+    filters.append(reset, categories, brands, prices, stock);
     return filters;
   }
 
